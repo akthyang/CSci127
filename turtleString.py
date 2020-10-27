2adb1f02fa9be6359a63371f349ebdc443d7e519
Alicia Yang <49996848+akthyang@users.noreply.github.com>
2020-10-27 09:59:56 -0400

    Update turtleString.py

--- turtleString.py
+++ turtleString.py
@@ -2,33 +2,45 @@
 #Email:      !YourEmailHere!
 #A program that uses command strings to control turtle drawing
 
-
 import turtle
 
+#assign name to turtle
 tess = turtle.Turtle()
-myWin = turtle.Screen()     #The graphics window
+#the graphics window
+myWin = turtle.Screen()
+
+#ask user for input
 commands = input("Please enter a command string: ")
 
+#list out the commands
 for ch in commands:
-    #perform action indicated by the character
-    if ch == 'F':            #move forward
-        tess.forward(50)
-    elif ch == 'L':          #turn left
-        tess.left(90)
-    elif ch == 'R':          #turn right
-        tess.right(90)
-    elif ch == '^':          #lift pen
-        tess.penup()
-    elif ch == 'v':          #lower pen
-        tess.pendown()
-    elif ch == 'r':          #turn red
-        tess.color("red")
-    elif ch == 'g':          #turn green
-        tess.color("green")
-    elif ch == 'b':          #turn blue
-        tess.color("blue")
-    else:                    #for any other character, print an error message
-        print("Error: do not know the command:", ch)
+    #perform action indicated by character
+    if ch == 'F':
+        tess.forward(50) #moves forward 50 steps
+    elif ch == 'L':
+        tess.left(90) #moves left 90
+    elif ch == 'R':
+        tess.right(90) #moves right 90
+    elif ch == '^':
+        tess.penup() #lifts pen
+    elif ch == 'v':
+        tess.pendown() #lowers pen
+    elif ch == 'B':
+        tess.backward(50) #moves backwards
+    elif ch == 'S':
+        tess.stamp() #makes a stamp
+    elif ch == 'D':
+        tess.dot() #stamps a dot
+    elif ch == 'p':
+        tess.color('purple') #turns purple
+    elif ch == 'g':
+        tess.color('green') #turns green
+    elif ch == 'b':
+        tess.color('blue') #turns blue
+    else: #for any other characters orint error
+        print("Error: do not know command:", c)
 
-print("See graphics window for your image")
-myWin.exitonclick()         #Close the window when clicked
+print("See graphics window for image")
+myWin.exitonclick() #close window when clicked
+
+
